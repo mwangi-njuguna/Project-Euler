3a27fd96d842d71914f18514352b0d1e5263283e
mwangi-njuguna <mwanginjuguna59@gmail.com>
2018-04-17 20:25:19 +0300

    Simplified the if statement using all()

--- smallest_multiple.py
+++ smallest_multiple.py
@@ -1,10 +1,10 @@
 def smallest_m():
-	start=1
-	while True:
-		if start%2==0 and start%3==0 and start%4==0 and start%5==0 and start%5==0 and start%6==0 and start%7==0 and start%8==0 and start%9==0 and start%10==0 and start%11==0 and start%12==0 and start%13==0 and start%14==0 and start%15==0 and start%16==0 and start%17==0 and start%18==0 and start%19==0 and start%20==0:
-			print(start)
-			break
-		else:
-			start+=1
-                
+    start = 1
+    while True:
+        if all(start % i == 0 for i in range(20, 1,-1)):
+            print(start)
+            break
+        else:
+            start+=1
+
 smallest_m()
